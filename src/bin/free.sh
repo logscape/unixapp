a0702d1ce30082dd8dfef167adb394f1ac326713
Zvenyika Gomo <zvenyika.gomo@excelian.com>
2013-08-27 12:26:13 +0100

    - cleaned configs

--- src/bin/free.sh
+++ src/bin/free.sh
@@ -2,8 +2,10 @@
 # 
 . `dirname $0`/common.sh
 
-if [ "x$KERNEL" = "xSunOS" ] ; then
-	`dirname $0`/free-sunOs.sh
-else
+echo $KERNEL
+
+if [ "x$KERNEL"="xLinux" ];then
 	`dirname $0`/free-all.sh
+elif [ "x$KERNEL" = "xSunOS" ] ; then
+	`dirname $0`/free-sunOs.sh
 fi
